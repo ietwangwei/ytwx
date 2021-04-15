c5624be9c607d1b2c82dc4c5396f83f680011d7c
程高伟 <peer44@qq.com>
2021-04-15 16:29:34 +0800

    上传文件大小限制为2M

--- src/pages/signup/index.vue
+++ src/pages/signup/index.vue
@@ -8,10 +8,7 @@
         @click="click(item)"
       >
         <div class="ceel-label">
-          <span
-            v-if="item.required"
-            style="color: red;"
-          >*</span>
+          <span v-if="item.required" style="color: red">*</span>
           <span class="margin-left10 title">{{ item.label }}</span>
         </div>
         <van-field
@@ -38,7 +35,7 @@
           :max-count="item.max"
           :after-read="afterRead"
           class="van-cell"
-          accept="image" 
+          accept="image"
         />
         <div
           v-if="item.type === 'area'"
@@ -51,11 +48,13 @@
       <div class="button">
         <van-checkbox
           v-model="checked"
-          style="margin-top: 12px;"
+          style="margin-top: 12px"
           icon-size="14px"
           checked-color="#07c160"
         >
-          <span style="color: rgba(69, 90, 100, 0.6);font-size: 14px;">我承诺所填信息均真实有效</span>
+          <span style="color: rgba(69, 90, 100, 0.6); font-size: 14px"
+            >我承诺所填信息均真实有效</span
+          >
         </van-checkbox>
         <van-button
           :disabled="submitDisabled || !checked"
@@ -63,7 +62,7 @@
           loading-text="正在提交..."
           loading-type="spinner"
           type="primary"
-          style="margin-top: 12px;"
+          style="margin-top: 12px"
           @click="submitData"
         >
           提 交
@@ -71,10 +70,7 @@
       </div>
     </van-cell-group>
     <!-- 选择特长 -->
-    <van-popup
-      v-model="show"
-      position="bottom"
-    >
+    <van-popup v-model="show" position="bottom">
       <van-picker
         v-model="formData.techang"
         :columns="columns"
@@ -84,10 +80,7 @@
       />
     </van-popup>
     <!-- 选择省市区 -->
-    <van-popup
-      v-model="areaShow"
-      position="bottom"
-    >
+    <van-popup v-model="areaShow" position="bottom">
       <van-area
         :area-list="areaList"
         :value="formData.householdCode"
@@ -105,7 +98,7 @@ export default {
   data() {
     return {
       submit: true,
-      submitStr:'',
+      submitStr: "",
       show: false,
       areaShow: false,
       checked: false,
@@ -116,7 +109,7 @@ export default {
           type: "field",
           prop: "studentName",
           required: true,
-          placeholder: "请输入姓名"
+          placeholder: "请输入姓名",
         },
         {
           label: "考生电话",
@@ -124,42 +117,42 @@ export default {
           mode: "number",
           prop: "telephone",
           required: true,
-          placeholder: "请输入联系电话"
+          placeholder: "请输入联系电话",
         },
         {
           label: "身份证号码",
           type: "field",
           prop: "idCardNumber",
           required: true,
-          placeholder: "请输入身份证号码"
+          placeholder: "请输入身份证号码",
         },
         {
           label: "户 籍",
           type: "area",
           prop: "household",
           required: true,
-          placeholder: "请输入户籍"
+          placeholder: "请输入户籍",
         },
         {
           label: "初中毕业学校（全称）",
           type: "field",
           prop: "graduateMiddleSchool",
           required: true,
-          placeholder: "请输入初中毕业学校（全称）"
+          placeholder: "请输入初中毕业学校（全称）",
         },
         {
           label: "报考特长",
           type: "picker",
           prop: "skill",
           required: true,
-          placeholder: "请输入报考特长"
+          placeholder: "请输入报考特长",
         },
         {
           label: "监护人姓名",
           type: "field",
           prop: "guarderName",
           required: true,
-          placeholder: "请输入监护人姓名"
+          placeholder: "请输入监护人姓名",
         },
         {
           label: "监护人电话",
@@ -167,21 +160,21 @@ export default {
           prop: "guarderTelephone",
           required: true,
           mode: "number",
-          placeholder: "请输入监护人电话"
+          placeholder: "请输入监护人电话",
         },
         {
           label: "考生照片（2寸证件照）",
           type: "upload",
           required: true,
           max: 1,
-          prop: "zjzUrls"
+          prop: "zjzUrls",
         },
         {
           label: "身份证或户口簿",
           type: "upload",
           required: true,
           max: 1,
-          prop: "sfzhkbUrls"
+          prop: "sfzhkbUrls",
         },
         {
           label: "获得的主要荣誉",
@@ -189,7 +182,7 @@ export default {
           prop: "honourUrls",
           required: false,
           max: 3,
-          placeholder: "请输入获得的主要荣誉"
+          placeholder: "请输入获得的主要荣誉",
         },
         {
           label: "艺术体育学习经历",
@@ -197,13 +190,13 @@ export default {
           prop: "studyExperience",
           required: false,
           mode: "textarea",
-          placeholder: "请输入艺术体育学习经历"
-        }
+          placeholder: "请输入艺术体育学习经历",
+        },
       ],
       formData: {
         studentName: "",
         household: "四川省,成都市,双流区",
-        householdCode: '510116',
+        householdCode: "510116",
         idCardNumber: "",
         graduateMiddleSchool: "",
         skill: "美术",
@@ -212,17 +205,17 @@ export default {
         zjzUrls: [],
         sfzhkbUrls: [],
         honourUrls: [],
-        studyExperience: ""
+        studyExperience: "",
       },
       currentPickerProp: "",
       currentPickerValue: "",
       columns: [],
       columnsMap: {
-        skill: ["美术", "体育", "声乐", "器乐", "舞蹈", "播音主持", "书法"]
+        skill: ["美术", "体育", "声乐", "器乐", "舞蹈", "播音主持", "书法"],
       },
       submitDisabled: true,
       uploadProp: null,
-      loading: false
+      loading: false,
     };
   },
   watch: {
@@ -245,12 +238,12 @@ export default {
           }
         }
         this.submitDisabled = result;
-        if(this.submit==false)  {
+        if (this.submit == false) {
           this.submitDisabled = true;
         }
       },
-      deep: true
-    }
+      deep: true,
+    },
   },
   mounted() {
     this.canSubmit();
@@ -258,34 +251,34 @@ export default {
     if (routeParams.hasData) {
       let data = localStorage.getItem("baoming");
       this.formData = JSON.parse(data);
-      this.formData.zjzUrls = this.formData.zjzUrls.map(i => {
+      this.formData.zjzUrls = this.formData.zjzUrls.map((i) => {
         return {
           url: i,
-          name: '测试'
-        }
+          name: "测试",
+        };
       });
-      this.formData.sfzhkbUrls = this.formData.sfzhkbUrls.map(i => {
+      this.formData.sfzhkbUrls = this.formData.sfzhkbUrls.map((i) => {
         return {
           url: i,
-          name: '测试'
-        }
+          name: "测试",
+        };
       });
-      this.formData.honourUrls = this.formData.honourUrls.map(i => {
+      this.formData.honourUrls = this.formData.honourUrls.map((i) => {
         return {
           url: i,
-          name: '测试'
-        }
+          name: "测试",
+        };
       });
     }
   },
   methods: {
-    canSubmit () {
-      this.$http.ytzx.canSubmit().then(res => {
+    canSubmit() {
+      this.$http.ytzx.canSubmit().then((res) => {
         if (res.code === 200) {
           console.info(res.data.submit);
           this.submit = res.data.submit;
           this.submitStr = res.data.message;
-          if (res.data.submit==false) {
+          if (res.data.submit == false) {
             this.$toast(res.data.message);
             this.submitDisabled = true;
           }
@@ -309,15 +302,31 @@ export default {
     },
     afterRead(e) {
       if (!e) return;
+      let size = e.file.size / 1024 / 1024;
+      console.info(size);
+      if (size > 2) {
+        let array = this.formData[this.uploadProp];
+        let newArray = [];
+        if (array.length > 0) {
+          array.forEach((element) => {
+            if (element.url) {
+              newArray.push(element);
+            }
+          });
+        }
+        this.formData[this.uploadProp] = newArray;
+        this.$toast("图片大小不能超过2M");
+        return;
+      }
       let formData = new FormData();
       formData.append("file", e.file);
       let len = this.formData[this.uploadProp].length;
       this.formData[this.uploadProp].splice([len - 1], 1);
-      this.$http.ytzx.upload(formData).then(res => {
+      this.$http.ytzx.upload(formData).then((res) => {
         if (res.code === 200) {
           this.formData[this.uploadProp].push({
-            url: res.data.url
-          })
+            url: res.data.url,
+          });
           this.uploadProp = null;
           this.$toast("上传成功");
         } else {
@@ -326,28 +335,28 @@ export default {
         }
       });
     },
-    getUuid () {
-      return new Date().getTime()
+    getUuid() {
+      return new Date().getTime();
     },
     submitData() {
-      if(!this.submit){
+      if (!this.submit) {
         this.$toast(this.submitStr);
         return;
       }
-      this.loading = true
+      this.loading = true;
       const queryData = Object.assign({}, this.formData, {
-        zjzUrls: this.formData.zjzUrls.map(i => i.url),
-        sfzhkbUrls: this.formData.sfzhkbUrls.map(i => i.url),
-        honourUrls: this.formData.honourUrls.map(i => i.url)
+        zjzUrls: this.formData.zjzUrls.map((i) => i.url),
+        sfzhkbUrls: this.formData.sfzhkbUrls.map((i) => i.url),
+        honourUrls: this.formData.honourUrls.map((i) => i.url),
       });
-      let openid = localStorage.getItem('openid');
-      this.$http.ytzx.saveBaoming(queryData).then(res => {
-        this.loading = false
+      let openid = localStorage.getItem("openid");
+      this.$http.ytzx.saveBaoming(queryData).then((res) => {
+        this.loading = false;
         if (res.code === 200) {
           this.$toast("报名成功！");
-          localStorage.clear()
+          localStorage.clear();
           this.$Taro.navigateTo({
-            url: `/pages/index/index?openid=${openid}&id=${this.getUuid()}`
+            url: `/pages/index/index?openid=${openid}&id=${this.getUuid()}`,
           });
         } else {
           this.$toast(res.message);
@@ -355,8 +364,8 @@ export default {
       });
     },
     onAreaConfirm(e) {
-      let str = e.map(i => i.name).join(",");
-      let code = e[2].code
+      let str = e.map((i) => i.name).join(",");
+      let code = e[2].code;
       this.formData.household = str;
       this.formData.householdCode = code;
       this.areaShow = false;
@@ -371,15 +380,15 @@ export default {
       if (item.type !== "upload") return;
       this.uploadProp = item.prop;
     },
-    pickerConfirmHandler (e, index) {
+    pickerConfirmHandler(e, index) {
       this.formData[this.currentPickerProp] = e;
       this.currentPickerProp = "";
-      this.show = false
+      this.show = false;
     },
-    clearHandler (item) {
-      this.formData[item.prop] = ''
-    }
-  }
+    clearHandler(item) {
+      this.formData[item.prop] = "";
+    },
+  },
 };
 </script>
 
